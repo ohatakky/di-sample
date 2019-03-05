82c25d5a4fbe21801e7c38b5cf0b91da40d68ab6
ohatakky <ohatakky@gmail.com>
2019-03-05 15:56:23 +0900

    saveメソッド実装

--- repository/user.go
+++ repository/user.go
@@ -9,7 +9,15 @@ type UserRepository interface {
 	Save(u model.User) error
 }
 
-func NewUserRepository(*sql.DB) UserRepository {
+type userRepository struct {
+	DB *sql.DB
+}
 
+func NewUserRepository(db *sql.DB) userRepository {
+
+	return userRepository{db}
+}
+
+func (u userRepository) Save(model.User) error {
 	return nil
 }
